87890ebfea27401f8b06788c85514c1b3851dc34
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-05-30 16:46:24 +0200

    impelement lcd screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mfrc522
-import utime # ignore: type
+from machine import I2C, Pin
+from lcd_i2c import LCD
 
 FIRST_BLOCK = 4
 BLOCK_COUNT = 64
@@ -70,6 +71,17 @@ def write(reader: mfrc522.MFRC522, uid: list, data: str):
         else:
             raise Exception("Unknown status %d" % status)
 
+# Initialize the I2C bus
+SCREEN_I2C_ADDR = 0x27     # DEC 39, HEX 0x27
+SCREEN_I2C_NUM_ROWS = 2
+SCREEN_I2C_NUM_COLS = 16
+
+screen_i2c = I2C(0, scl=Pin(5), sda=Pin(4), freq=800000)
+lcd = LCD(i2c=screen_i2c, addr=SCREEN_I2C_ADDR, cols=SCREEN_I2C_NUM_COLS, rows=SCREEN_I2C_NUM_ROWS)
+
+lcd.begin()
+
+# initialize the RFID reader
 reader = mfrc522.MFRC522(sck="GP18", mosi="GP19", miso="GP16", rst="GP17", cs="GP20")
 previous_uid = None
 
@@ -97,6 +109,8 @@ try:
         previous_uid = uid
         if(current_mode == "read"):
             data = read(reader, uid)
+            lcd.clear()
+            lcd.print(data)
             print("Data: %s" % data)
         elif(current_mode == "write"):
             data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent."
